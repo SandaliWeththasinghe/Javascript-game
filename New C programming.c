f8ca71b4ef94238afdd8bbde333281c38f2748e8
Sandali Weththasinghe <sndlz97@gmail.com>
2018-06-26 14:56:55 +0530

    Javascript Game

---
+++ New C programming.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+//this program is used to print a character
+
+void main()
+
+{
+
+    char c='A';
+	clrscr();
+	printf("Character is %c",c);
+	
+	getch();
+	
+}
